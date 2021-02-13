961f299aca1f194d30f0969f049b6bbbb0d76a22
Ganeshdevops <ganeshkadiri@gmail.com>
2021-02-13 17:32:26 +0530

    variable shell

--- project/read.sh
+++ project/read.sh
@@ -1,17 +1,7 @@
-#!bin/bash
 #!/bin/bash
-name =ganesh
-echo name =$name
 
-count =2021
+read -p 'Enter your Name: ' name
+read -p 'Enter your age: ' age
 
-echo ="wellcome to $name at $count"
 
-echo -e "hai\nhellow\n$name"
-
-echo "hellow \t\t\t$name\t\t%$count"
-
-echo -p 'enter your name:' name
-echo -p 'enter your birth:' count
-
-echo -e "hellow name =$name\n enter birth =$count "
+echo -e "\n Your Name = $name\n Your Age = $age"
